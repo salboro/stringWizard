92eb9d6384dcf60211d7c973717f45ed3fed0e73
vlasov <n.vlasov@cft.ru>
2024-08-14 17:06:21 +0300

    add example action

---
+++ src/main/kotlin/com/string/wizard/stringwizard/ui/Icons.kt
@@ -0,0 +1,8 @@
+package com.string.wizard.stringwizard.ui
+
+import com.intellij.openapi.util.IconLoader
+
+object Icons {
+    @JvmField
+    val KabanSmall = IconLoader.getIcon("META-INF/icons/kabanIconSmall.svg", javaClass)
+}
