431d8c2ade13ff540475b7257fd15f626e22cdc3
vlasov <n.vlasov@cft.ru>
2024-12-03 16:58:33 +0300

    Добавил селектор excel файла

--- src/main/kotlin/com/string/wizard/stringwizard/presentation/addfromexcel/StringAddFromExcelPresenter.kt
+++ src/main/kotlin/com/string/wizard/stringwizard/presentation/addfromexcel/StringAddFromExcelPresenter.kt
@@ -1,6 +1,11 @@
 package com.string.wizard.stringwizard.presentation.addfromexcel
 
+import com.intellij.openapi.vfs.VirtualFile
 import com.string.wizard.stringwizard.ui.addfromexcel.StringAddFromExcelUi
 
 class StringAddFromExcelPresenter(private val ui: StringAddFromExcelUi) {
+
+	fun chooseFile(file: VirtualFile) {
+
+	}
 }
