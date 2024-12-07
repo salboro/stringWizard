8f2cfc779a0453f585901eee73c23bba25197177
vlasov <n.vlasov@cft.ru>
2024-12-07 23:29:13 +0300

    Поправил сообщение об успешном копировании строки

--- src/main/kotlin/com/string/wizard/stringwizard/ui/util/StringFormatUtil.kt
+++ src/main/kotlin/com/string/wizard/stringwizard/ui/util/StringFormatUtil.kt
@@ -6,9 +6,16 @@ fun formatResourceString(name: String, value: String, locale: Locale): String =
 	"<html><b><font color=#7EDC95>name:</b></font> $name       <font color=#7EDC95><b>value:</b></font> $value       <font color=#7EDC95><b>locale:</b></font> ${locale.name}</html>"
 
 fun copySuccessMessageFormat(sourceModuleName: String, targetModuleName: String, sourceStringName: String, newStringName: String): String =
-	"<html>String <b><font color=#7EDC95>$sourceStringName</b></font> successfully copied from module <b><font color=#7EDC95>$sourceModuleName</b></font>" +
-		" into module <b><font color=#7EDC95>$targetModuleName</b></font> with name <b><font color=#7EDC95>$newStringName</b></font><html>"
-
+	"""
+		|<html>
+		|<br>String successfully copied! 
+		|<br>Info:
+		|<br>	Source string: <b><font color=#7EDC95>$sourceStringName</b></font>
+		|<br>	Source module: <b><font color=#7EDC95>$sourceModuleName</b></font>
+		|<br>	Target module: <b><font color=#7EDC95>$targetModuleName</b></font>
+		|<br>	New string name: <b><font color=#7EDC95>$newStringName</b></font>
+		|<html>
+	    """.trimMargin()
 
 fun formatExcelString(value: String, position: Int, locale: Locale): String =
 	"<html><b><font color=#7EDC95>value:</b></font> $value       <font color=#7EDC95><b>position:</b></font> $position       <font color=#7EDC95><b>locale:</b></font> ${locale.name}</html>"
