c063bcd2b02e21d6eae3cad2db1889560eb54f26
vlasov <n.vlasov@korona.net>
2025-03-18 14:48:15 +0300

    Добавил тудуху на правку в сущности NewString

--- src/main/kotlin/com/string/wizard/stringwizard/domain/entity/NewString.kt
+++ src/main/kotlin/com/string/wizard/stringwizard/domain/entity/NewString.kt
@@ -2,4 +2,10 @@ package com.string.wizard.stringwizard.domain.entity
 
 import com.string.wizard.stringwizard.data.entity.Locale
 
+/*
+	TODO: Поле position нигде не используется.
+
+	Подумать о переносе этой сущности на presentation слой.
+	Кажется, что её существование обусловлено структурой ui, поэтому она где-то там и должна быть
+ */
 data class NewString(val value: String, val position: Int, val locale: Locale)
